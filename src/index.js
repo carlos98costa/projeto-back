fdc78a1b905be44af92f5f055f65d490df9f7b1f
carlos98costa <46066793+carlos98costa@users.noreply.github.com>
2026-05-13 13:02:14 -0300

    senver: backend usa versao do env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const version = process.env.VERSION || process.env.npm_package_version || '1.0.0';
 
 app.use(cors());
 app.use(express.json());
